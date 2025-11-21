427303c93886c5002da168668339997a69a904dd
HoangLong-88 <quoccuong130406@gmail.com>
2025-11-21 16:10:26 +0700

    test create migration

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisterUserController;
+use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [RegisterUserController::class, 'store']);
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
