df474fb43c00feebab1dcd1f0f9deca02f43a5de
HoangLong-88 <quoccuong130406@gmail.com>
2025-12-07 20:32:54 +0700

    fix conflict merge

---
+++ database/data/attractions.php
@@ -0,0 +1,48 @@
+<?php
+return [
+    [
+        'atractionName'   => 'Thả lồng đèn trên sông ban đêm Hội An',
+        'specificType'   => 'Lễ hội lồng đèn',
+        'category'      => 'tour',
+        'duration'     => '45 phút - 1 tiếng',
+        'image'      => 'https://thanhnien.mediacdn.vn/Uploaded/minhnguyet/2022_11_05/le-hoi-den-7431.jpg',
+        'price'      => 99000,
+        'rate'     => 4.9,
+    ],
+    [
+        'atractionName'   => 'Lướt ván trên biển Mỹ Khê',
+        'specificType'   => 'Thể thao ngoài trời',
+        'category'      => 'hoạt động ngoài trời',
+        'duration'     => '45 phút - 1 tiếng',
+        'image'      => 'https://www.vietrekking.com/wp-content/uploads/2021/03/luot-song-mui-ne-1280x720.jpg',
+        'price'      => 99000,
+        'rate'     => 4.9,
+    ],
+    [
+        'atractionName'   => 'Di tích khu K20',
+        'specificType'   => 'Di tích lịch sử',
+        'category'      => 'bảo tàng',
+        'duration'     => '45 phút - 1 tiếng',
+        'image'      => 'https://nguhanhson.org/wp-content/uploads/2017/07/ak204.jpg',
+        'price'      => 99000,
+        'rate'     => 4.9,
+    ],
+    [
+        'atractionName'   => 'Áo Dài Show Đà Nẵng',
+        'specificType'   => 'Chương trình biểu diễn',
+        'category'      => 'show',
+        'duration'     => '45 phút - 1 tiếng',
+        'image'      => 'https://danangfantasticity.com/wp-content/uploads/2022/04/ao-dai-show-tai-danang.jpg',
+        'price'      => 99000,
+        'rate'     => 4.9
+    ],
+    [
+        'atractionName'   => 'Lớp nấu món ăn VN tại Đà Nẵng',
+        'specificType'   => 'Lớp kĩ năng',
+        'category'     => 'lớp học',
+        'duration'     => '45 phút - 1 tiếng',
+        'image'      => 'https://storage.googleapis.com/fvallimages/uploads/tour/authentic-vietnamese-cooking-class-da-nang-highlight-tour-3321.jpg',
+        'price'      => 99000,
+        'rate'     => 4.9
+    ],
+];
--- database/data/stays.php
+++ database/data/stays.php
@@ -5,7 +5,7 @@ return [
         'location'   => 'Đà Nẵng',
         'address'    => '50 Bach Dang St, Hai Chau District, Sông Hàn, Đà Nẵng',
         'image'      => 'https://th.bing.com/th/id/R.102644a9d897e34d7648b61caed9fa71?rik=myKPuKNPDr%2bXBA&pid=ImgRaw&r=0',
-        'price'      => 999.000,
+        'price'      => 999000,
         'rating'     => 4.9,
     ],
     [
@@ -13,7 +13,7 @@ return [
         'location'   => 'Đà Nẵng',
         'address'    => '232 Vo Nguyen Giap St, Ngu Hanh Son Ward, Da Nang',
         'image'      => 'https://pix10.agoda.net/hotelImages/861432/-1/1c1abb90c28013840d46e9ef77ced95f.jpg?s=1024x768',
-        'price'      => 999.000,
+        'price'      => 999000,
         'rating'     => 4.9,
     ],
     [
@@ -21,7 +21,7 @@ return [
         'location'   => 'Đà Nẵng',
         'address'    => '277 Vo Nguyen Giap St, Ngu Hanh Son Ward, Da Nang',
         'image'      => 'https://indochinatreks.com/wp-content/uploads/2022/12/Haian-h-1swim.jpg',
-        'price'      => 999.000,
+        'price'      => 999000,
         'rating'     => 4.9,
     ],
     [
@@ -29,7 +29,7 @@ return [
         'location'   => 'Đà Nẵng',
         'address'    => '97-99 Duong Dinh Nghe, An Hai, Da Nang',
         'image'      => 'https://th.bing.com/th/id/R.102644a9d897e34d7648b61caed9fa71?rik=myKPuKNPDr%2bXBA&pid=ImgRaw&r=0',
-        'price'      => 999.000,
+        'price'      => 999000,
         'rating'     => 4.9
     ],
     [
@@ -37,7 +37,7 @@ return [
         'location'   => 'Đà Nẵng',
         'address'    => '258 Vo Nguyen Giap St, Ngu Hanh Son Ward, Da Nang',
         'image'      => 'https://th.bing.com/th/id/R.102644a9d897e34d7648b61caed9fa71?rik=myKPuKNPDr%2bXBA&pid=ImgRaw&r=0',
-        'price'      => 999.000,
+        'price'      => 999000,
         'rating'     => 4.9
     ],
 ];
