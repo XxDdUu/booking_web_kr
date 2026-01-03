03038fe1c96d1c648c64518fb5fc5d05f0264abf
HoangLong-88 <quoccuong130406@gmail.com>
2026-01-03 18:41:00 +0700

    remove location keywords

--- app/Http/Controllers/Api/LocationController.php
+++ app/Http/Controllers/Api/LocationController.php
@@ -20,14 +20,4 @@ class LocationController extends Controller
             'locations' => $service->getHomepageLocations()
         ]);
     }
-    public function keywords(Request $request, LocationService $service)
-    {
-        $keyword = $request->query('q', '');
-
-        $getKeyWords = $service->getLocationKeyWord($keyword);
-
-        return response()->json(
-            $getKeyWords->pluck('locationName')->toArray()
-        );
-    }
 }
